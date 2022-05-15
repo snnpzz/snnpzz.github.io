05fc7f947725bc51a2285d90c4bb8828465e870b
Susanna Pozzoli <7399998+snnpzz@users.noreply.github.com>
2022-05-15 20:54:36 +0200

    Update pages.js

--- public/js/pages.js
+++ public/js/pages.js
@@ -1,3 +1,3 @@
-particlesJS.load("particles-js", "/particlesjs-config.json", function() {
+particlesJS.load("particles-js", "/public/particlesjs-config.json", function() {
   console.log("[particles.js] Loaded particlesjs-config.json");
 });
